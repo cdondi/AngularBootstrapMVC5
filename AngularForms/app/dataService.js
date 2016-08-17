11d32564b29473876729e9bf7f0266d2f1dfb5d9
Clive Dondi <clivedondi@hotmail.com>
2016-08-17 16:33:26 -0500

    First Commit

---
+++ AngularForms/app/dataService.js
@@ -0,0 +1,33 @@
+﻿
+
+angularFormsApp.factory('DataService',
+    function () {
+        var getEmployee = function (id) {
+            if (id == 123) {
+                return {
+                    fullName: "Milton Waddams",
+                    notes: "The ideal employee. Just don't touch his red staple.",
+                    department: "Administration",
+                    perkCar: true,
+                    perkStock: false,
+                    perkSixWeeks: true,
+                    payrollType: "none"
+                };
+            }
+            return undefined;
+        };
+
+        var insertEmployee = function (newEmployee) {
+            return true;
+        };
+
+        var updateEmployee = function (employee) {
+            return true;
+        };
+
+        return {
+            insertEmployee: insertEmployee,
+            updateEmployee: updateEmployee,
+            getEmployee: getEmployee
+        };
+    });
